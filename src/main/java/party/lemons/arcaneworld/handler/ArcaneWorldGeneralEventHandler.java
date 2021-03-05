54031576d3493bed5de5ae2b8f1277d91462c36d
Colin Wong <cwongmath@gmail.com>
2021-03-04 19:46:02 -0600

    Fix scroll chance error

--- src/main/java/party/lemons/arcaneworld/handler/ArcaneWorldGeneralEventHandler.java
+++ src/main/java/party/lemons/arcaneworld/handler/ArcaneWorldGeneralEventHandler.java
@@ -36,7 +36,7 @@ public final class ArcaneWorldGeneralEventHandler {
         if(event.getEntity() instanceof AbstractIllager)
         {
             Entity e = event.getEntity();
-            if(e.world.rand.nextInt(ArcaneWorldConfig.ENTITIES.SCROLL_CHANCE) == 0)
+            if(e.world.rand.nextInt(100) < ArcaneWorldConfig.ENTITIES.SCROLL_CHANCE)
             {
                 EntityItem item = new EntityItem(e.world, e.posX, e.posY, e.posZ, ArcaneWorldUtil.getRandomRitualScrollForDrop(e.world.rand));
                 event.getDrops().add(item);
